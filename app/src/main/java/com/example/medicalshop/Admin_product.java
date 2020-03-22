4e9dbafeb20e398560d38a3c163192aeb4af58ad
Shivani <ladshivani93@gmail.com>
2020-03-23 01:48:32 +0530

    Added admin category nd product java

--- app/src/main/java/com/example/medicalshop/Admin_product.java
+++ app/src/main/java/com/example/medicalshop/Admin_product.java
@@ -1,14 +1,30 @@
 package com.example.medicalshop;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Admin_product extends AppCompatActivity {
+public class Admin_product extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_product);
+        Spinner prospin=findViewById(R.id.prodropdown);
+        prospin.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
